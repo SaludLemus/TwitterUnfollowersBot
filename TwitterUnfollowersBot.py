59c3f84d05fd5553d664d815fee3cbb7d650e812
Salud Lemus <saludlemus1@gmail.com>
2020-02-11 20:34:22 -0800

    Updated comments
    Also removed the wait time between inputting the user's username/email
    and password.

--- TwitterUnfollowersBot.py
+++ TwitterUnfollowersBot.py
@@ -12,13 +12,14 @@ from selenium import webdriver
 
 TWITTER_LOGIN_URL = 'https://twitter.com/login'
 
+# How long to wait for the page to load (in seconds).
 LOAD_TIME = 4
 
 #------------------------
 # Used for the login page.
 XPATH_USERNAME_INPUT = '//input[@name="session[username_or_email]"]'
 XPATH_PASSWORD_INPUT = '//input[@name="session[password]"]'
-XPATH_LOGIN = '//div[@role="button"]'
+XPATH_LOGIN_BUTTON = '//div[@role="button"]'
 #------------------------
 
 
@@ -40,15 +41,13 @@ class TwitterBot:
         self.driver.find_element_by_xpath(
                 XPATH_USERNAME_INPUT).send_keys(self.username)
 
-        time.sleep(LOAD_TIME)
-
         # Input the user's password.
         self.driver.find_element_by_xpath(
                 XPATH_PASSWORD_INPUT).send_keys(self.password)
 
         # Now, the `Log in` button should be clickable to actually sign into the
         # account.
-        self.driver.find_element_by_xpath(XPATH_LOGIN).click()
+        self.driver.find_element_by_xpath(XPATH_LOGIN_BUTTON).click()
 
 
 def main():
